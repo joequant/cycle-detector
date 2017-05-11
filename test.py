f23f31423c6cd4ff2112754fad1a93f847b7ef7d
Joseph C Wang <joequant@gmail.com>
2017-05-12 03:57:06 +0800

    add initial files

---
+++ test.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+import bellmanford
+import csv
+import sys
+import math
+graph = {}
+
+def add_link(f,t,v):
+    if f not in graph:
+        graph[f] = {}
+    graph[f][t] = v
+
+with open(sys.argv[1], 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for row in reader:
+        format = row[0]
+        if format == 'link':
+            f = row[1]
+            t = row[2]
+            v = float(row[3])
+            add_link(f,t,v)
+        elif format == 'node':
+            f = row[1]
+            if f not in graph:
+                graph[f] = {}
+        elif format == 'bid-ask':
+            f = row[1]
+            t = row[2]
+            b = math.log(float(row[3]))
+            a = math.log(float(row[4]))
+            add_link(f,t,-b)
+            add_link(t,f,a)
+        elif format == 'fee':
+            f = row[1]
+            t = row[2]
+            v = math.log(float(row[3]))
+            add_link(f,t,v)
+            add_link(t,f,v)
+
+print(graph)
+d, p = bellmanford.bellman_ford(graph, 'a')
+print (d, p)
