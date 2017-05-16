35e205d111e8dcebbca4de9dc0567565659da22b
Joseph C Wang <joequant@gmail.com>
2017-05-16 12:28:45 +0800

    add react bootstrap

--- cycleweb-react/src/App.js
+++ cycleweb-react/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Button} from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,7 +10,8 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
-        </div>
+            </div>
+	    <Button />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
