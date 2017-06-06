b0326a9c4021f5f27be8e4fb714b7afb34d97835
Joseph C Wang <joequant@gmail.com>
2017-06-06 20:41:54 +0800

    fix run.sh so that old file gets overwritten only when complete

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 while true;
 do
 ../bitcoin-price-api/scripts/dumpprices.py > ./cycleweb-react/build/static/prices.csv
-./cycledetect.py  ./cycleweb-react/build/static/*.csv > ./cycleweb-react/build/static/arb.txt
+./cycledetect.py  ./cycleweb-react/build/static/*.csv > arb.txt
+mv arb.txt ./cycleweb-react/build/static/arb.txt
 sleep 15
 done
  
