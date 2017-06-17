9dd57e27aaf99d28bd0f6fce7c366bfe122a06d2
Joseph C Wang <joequant@gmail.com>
2017-06-17 21:03:53 +0800

    don't overwrite prices

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 while true;
 do
-../bitcoin-price-api/scripts/dumpprices.py > ./cycleweb-react/build/static/prices.csv
+../bitcoin-price-api/scripts/dumpprices.py > prices.csv
+mv prices.csv ./cycleweb-react/build/static/prices.csv
 ./cycledetect.py  ./cycleweb-react/build/static/transaction.csv \
 		  ./cycleweb-react/build/static/prices.csv > arb.txt
 mv arb.txt ./cycleweb-react/build/static/arb.txt
