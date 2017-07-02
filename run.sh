f95417315cf3fecacdc5ed6343a6167fc4404f05
Joseph C Wang <joequant@gmail.com>
2017-07-02 11:52:23 +0800

    convert to use dir

--- run.sh
+++ run.sh
@@ -4,8 +4,9 @@ while true;
 do
 ../bitcoin-price-api/scripts/dumpprices.py > prices.csv
 mv prices.csv ./cycleweb-react/build/static/prices.csv
-./cycledetect.py  ./cycleweb-react/build/static/transaction.csv \
-		  ./cycleweb-react/build/static/prices.csv > arb.txt
+./cycledetect.py --dir ./cycleweb-react/build/static \
+		 transaction.csv \
+		 prices.csv > arb.txt
 mv arb.txt ./cycleweb-react/build/static/arb.txt
 sleep 15
 done
