c43669bfe6d970fa755b45a4e5256f72d0a3f59d
Joseph C Wang <joequant@gmail.com>
2017-07-05 01:45:18 +0800

    split up run

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ while true;
 do
 ../bitcoin-price-api/scripts/dumpprices.py > prices.csv
 mv prices.csv ./cycleweb-react/build/static/prices.csv
-./cycledetect.py --dir ./cycleweb-react/build/static \
-		 transaction.csv \
-		 prices.csv > arb.txt
-mv arb.txt ./cycleweb-react/build/static/arb.txt
+./cycledetect.py --dir ./cycleweb-react/build/static arb-all.csv > arb-all.txt
+mv arb-all.txt ./cycleweb-react/build/static/arb-all.txt
+./cycledetect.py --dir ./cycleweb-react/build/static arb-hk.csv > arb-hk.txt
+mv arb-hk.txt ./cycleweb-react/build/static/arb-hk.txt
 sleep 15
 done
- 
+
