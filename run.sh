6daa0e3ac41c50e6f6af9a18b70c0fb989515fc3
Joseph C Wang <joequant@gmail.com>
2017-07-14 12:50:13 +0800

    add timeout

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+killall ./cycleweb.py
 ./cycleweb.py &
 while true;
 do
-../bitcoin-price-api/scripts/dumpprices.py > prices.csv
+echo "Running at " $(date)
+timeout 30 ../bitcoin-price-api/scripts/dumpprices.py > prices.csv
 mv prices.csv ./cycleweb-react/build/static/prices.csv
 ./cycledetect.py --dir ./cycleweb-react/build/static arb-all.csv > arb-all.txt
 mv arb-all.txt ./cycleweb-react/build/static/arb-all.txt
